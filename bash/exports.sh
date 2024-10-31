9608c12c53f751428b38b607ea5eec5137cf7afb
Nicolas Gonzalez <ndgfrs@gmail.com>
2024-10-31 02:29:35 -0400

    feat: change `:-` to `:=` in all XDG exports

--- bash/exports.sh
+++ bash/exports.sh
@@ -4,9 +4,9 @@
 # to any subprocesses (subshells, other commands, etc.)
 
 # https://specifications.freedesktop.org/basedir-spec/latest/
-export XDG_CONFIG_HOME="${XDG_DATA_HOME:-$HOME/.config}"
-export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
-export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
-export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_CONFIG_HOME="${XDG_DATA_HOME:=$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:=$HOME/.local/share}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:=$HOME/.local/state}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:=$HOME/.cache}"
 
 export EDITOR='/usr/bin/nvim'
