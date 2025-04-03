9b691ad995af85cc2c58c883e4a4ea653359271e
Nicolas Gonzalez <ndgfrs@gmail.com>
2025-04-02 22:18:50 -0400

    fix: change ruff to ruff_format

--- nvim/init.lua
+++ nvim/init.lua
@@ -620,7 +620,7 @@ require("lazy").setup({
             end,
             formatters_by_ft = {
                 lua = { "stylua" },
-                python = { "isort", "ruff" },
+                python = { "isort", "ruff_format" },
                 -- You can use 'stop_after_first' to run the first available formatter from the list
                 -- javascript = { "prettierd", "prettier", stop_after_first = true },
             },
